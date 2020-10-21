fb8947cffbeecd0f2c16f446efe338203e4d1d36
adarvin14 <amdarvin14@gmail.com>
2020-10-21 17:19:31 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Alex Darvin"
+    end
+
+    get '/hometown' do
+        "My hometown is Short Hills"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Mind Over Matter by Young The Giant"
+    end
+
 end
